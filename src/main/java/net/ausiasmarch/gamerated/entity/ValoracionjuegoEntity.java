75d05e513c5f19508be72cdb98eaf0e94f206141
ximosoler <xsoler98@gmail.com>
2023-04-04 11:11:19 +0200

    Runea + cambios

--- src/main/java/net/ausiasmarch/gamerated/entity/ValoracionjuegoEntity.java
+++ src/main/java/net/ausiasmarch/gamerated/entity/ValoracionjuegoEntity.java
@@ -1,7 +1,5 @@
 package net.ausiasmarch.gamerated.entity;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
--- src/main/java/net/ausiasmarch/gamerated/repository/UsuarioRepository.java
+++ src/main/java/net/ausiasmarch/gamerated/repository/UsuarioRepository.java
@@ -1,9 +1,10 @@
 package net.ausiasmarch.gamerated.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import net.ausiasmarch.gamerated.entity.UsuarioEntity;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
     boolean existsByNick(String nick);
